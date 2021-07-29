9c2c983c41354cd614d6f548ae7fd28daaf8d4b5
Ben Cook <mcbeanoo@gmail.com>
2021-07-29 20:15:55 +1000

    initialise firebase

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "mernda-express.firebaseapp.com",
+  projectId: "mernda-express",
+  storageBucket: "mernda-express.appspot.com",
+  messagingSenderId: "514264266957",
+  appId: "1:514264266957:web:32f6da38179442ecaa5184",
+  measurementId: "G-GL88MCNDP8",
+};
