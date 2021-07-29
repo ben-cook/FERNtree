d1608c9e50dbfa51b854f499855e5ccad6285282
Ben Cook <mcbeanoo@gmail.com>
2021-07-29 20:20:15 +1000

    Add firebase (#1)
    * initialise firebase
    
    * add comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import "./App.css";
+import firebase from "firebase";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
 const App = () => {
+  // Get a refernce to the 'example' collection of documents
+  const exampleCollectionReference = firebase.firestore().collection("example");
+
+  // Get all the documents in the collection
+  const [examples] = useCollectionData(exampleCollectionReference);
+
   return (
     <div className="App">
       <h1>Mernda Express!</h1>
+
+      {/* Map over ever document in the collection and display it on the page */}
+      {examples?.map((example) => (
+        <p>{example.testField}</p>
+      ))}
     </div>
   );
 };
