9eb43170ce6e92adaf6fab1c9df87386adcb978d
Ben Cook <mcbeanoo@gmail.com>
2021-08-16 23:23:46 +1000

    add basic cards to homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,19 +1,57 @@
+import {
+  Card,
+  Grid,
+  makeStyles,
+  createStyles,
+  CardContent
+} from "@material-ui/core";
 import firebase from "firebase";
-import { useCollectionData } from "react-firebase-hooks/firestore";
+import {
+  useCollectionData,
+  useDocumentData
+} from "react-firebase-hooks/firestore";
+
+const useStyles = makeStyles({
+  grid: {
+    marginTop: "1rem"
+  }
+});
 
 const Home = () => {
-  // Get a refernce to the 'example' collection of documents
-  const exampleCollectionReference = firebase.firestore().collection("example");
+  const classes = useStyles();
 
-  // Get all the documents in the collection
-  const [examples] = useCollectionData(exampleCollectionReference);
+  const userDataReference = firebase
+    .firestore()
+    .collection("users")
+    .doc("RrHsrR19CIoCI3vUCWet");
+
+  const [userData] = useDocumentData(userDataReference);
+
+  const clientsReference = firebase
+    .firestore()
+    .collection("users")
+    .doc("RrHsrR19CIoCI3vUCWet")
+    .collection("clients");
+
+  const [clientsData] = useCollectionData(clientsReference);
 
   return (
     <>
-      {/* Map over ever document in the collection and display it on the page */}
-      {examples?.map((example, idx) => (
-        <p key={idx}>{example.testField}</p>
-      ))}
+      {clientsData && (
+        <Grid direction="column" container spacing={1} className={classes.grid}>
+          {clientsData.map((client, idx) => (
+            <Grid item key={idx}>
+              <Card variant="outlined">
+                <CardContent>
+                  {client.firstName} {client.lastName}
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      )}
+
+      <pre>{JSON.stringify(clientsData, null, 2)}</pre>
     </>
   );
 };
