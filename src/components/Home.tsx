d0d8b4157ecfd7456246534e2dbccd745dd8354b
Ben Cook <mcbeanoo@gmail.com>
2021-08-19 17:14:06 +1000

    add a search field to home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,31 +1,36 @@
 import {
   Card,
-  Grid,
-  makeStyles,
+  CardContent,
   createStyles,
-  CardContent
+  Grid,
+  IconButton,
+  InputAdornment,
+  makeStyles,
+  TextField,
+  MenuItem,
+  FormGroup,
+  FormControlLabel,
+  Checkbox
 } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 import firebase from "firebase";
-import {
-  useCollectionData,
-  useDocumentData
-} from "react-firebase-hooks/firestore";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
-const useStyles = makeStyles({
-  grid: {
-    marginTop: "1rem"
-  }
-});
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    searchCard: {
+      marginTop: "1rem",
+      backgroundColor: theme.palette.primary.light
+    },
+    clientSearchField: {},
+    grid: {
+      marginTop: "1rem"
+    }
+  })
+);
 
 const Home = () => {
   const classes = useStyles();
-
-  const userDataReference = firebase
-    .firestore()
-    .collection("users")
-    .doc("RrHsrR19CIoCI3vUCWet");
-
-  const [userData] = useDocumentData(userDataReference);
 
   const clientsReference = firebase
     .firestore()
@@ -35,12 +40,94 @@ const Home = () => {
 
   const [clientsData] = useCollectionData(clientsReference);
 
+  const labels = [
+    {
+      value: "All",
+      label: "All"
+    },
+    {
+      value: "EUR",
+      label: "€"
+    },
+    {
+      value: "BTC",
+      label: "฿"
+    },
+    {
+      value: "JPY",
+      label: "¥"
+    }
+  ];
+
   return (
     <>
+      <Card variant="outlined" className={classes.searchCard}>
+        <CardContent>
+          <Grid container direction="row" spacing={1}>
+            {/* Search bar and filters */}
+            <Grid container direction="column" item xs={12} sm={10}>
+              <Grid item>
+                <TextField
+                  variant="outlined"
+                  label="Find your Client"
+                  className={classes.clientSearchField}
+                  fullWidth
+                  margin="normal"
+                  size="medium"
+                  InputProps={{
+                    style: { backgroundColor: "white" },
+                    endAdornment: (
+                      <InputAdornment component="div" position="end">
+                        <IconButton>
+                          <SearchIcon />
+                        </IconButton>
+                      </InputAdornment>
+                    )
+                  }}
+                />
+              </Grid>
+
+              <Grid item>
+                <FormGroup row>
+                  {labels.map((label) => (
+                    <FormControlLabel
+                      key={label.value}
+                      control={<Checkbox color="primary" />}
+                      label={label.label}
+                    />
+                  ))}
+                </FormGroup>
+              </Grid>
+            </Grid>
+
+            {/* Selection and buttons */}
+            <Grid item xs={12} sm={2}>
+              <TextField
+                variant="outlined"
+                className={classes.clientSearchField}
+                fullWidth
+                margin="normal"
+                size="medium"
+                InputProps={{
+                  style: { backgroundColor: "white" }
+                }}
+                select
+              >
+                {labels.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+
       {clientsData && (
-        <Grid direction="column" container spacing={1} className={classes.grid}>
+        <Grid container spacing={1} className={classes.grid}>
           {clientsData.map((client, idx) => (
-            <Grid item key={idx}>
+            <Grid item key={idx} xs={12} sm={6} md={4}>
               <Card variant="outlined">
                 <CardContent>
                   {client.firstName} {client.lastName}
