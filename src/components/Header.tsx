e7cbe6c86df86556adf05f16947f8a7c6108f298
Ben Cook <mcbeanoo@gmail.com>
2021-08-19 17:02:09 +1000

    Add icons to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,66 +1,54 @@
 import {
-  Button,
   IconButton,
   Toolbar,
-  Typography,
-  AppBar
+  AppBar,
+  Container,
+  Grid
 } from "@material-ui/core";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import EcoIcon from "@material-ui/icons/Eco";
-import MenuIcon from "@material-ui/icons/Menu";
-import firebase from "firebase/app";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      flexGrow: 1
-    },
-    menuButton: {
+    profileButton: {
       marginRight: theme.spacing(2)
     },
-    title: {
-      flexGrow: 1
+    logoIcon: {
+      marginLeft: theme.spacing(2)
     }
   })
 );
 
 const Header = () => {
   const classes = useStyles();
-  const history = useHistory();
 
-  const [user] = useAuthState(firebase.auth());
+  const LogoIcon = () => (
+    <IconButton color="inherit" size="medium" className={classes.logoIcon}>
+      <EcoIcon fontSize="large" />
+    </IconButton>
+  );
+
+  const ProfileButton = () => (
+    <IconButton color="inherit" size="medium" className={classes.profileButton}>
+      <AccountCircleIcon fontSize="large" />
+    </IconButton>
+  );
 
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton
-          edge="start"
-          className={classes.menuButton}
-          color="inherit"
-          aria-label="menu"
-        >
-          <MenuIcon />
-        </IconButton>
-
-        <Typography
-          variant="h6"
-          className={classes.title}
-          onClick={() => history.push("/")}
-        >
-          Ferntree
-          <EcoIcon />
-        </Typography>
-
-        {/* We could use the Link component here, but I forget how to do this without messing up the styling */}
-        {/* <Link component={Button} to="/signup">
-          Sign Up
-        </Link> */}
-        {/* This is bad for accessibility, have to change this soon */}
-        <Button color="inherit" onClick={() => history.push("/account")}>
-          {user?.email ? user.email : "Sign In"}
-        </Button>
+        <Container>
+          <Grid container justifyContent={"space-between"}>
+            <Grid item>
+              <Link component={LogoIcon} to="/" />
+            </Grid>
+            <Grid item>
+              <Link component={ProfileButton} to="/signup" />
+            </Grid>
+          </Grid>
+        </Container>
       </Toolbar>
     </AppBar>
   );
