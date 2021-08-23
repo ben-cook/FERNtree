6fba4de37ff523c73b148b71a62e7ab0c4e6b31b
Ben Cook <mcbeanoo@gmail.com>
2021-08-23 15:19:41 +1000

    change the theme up slightly

--- src/theme.ts
+++ src/theme.ts
@@ -3,16 +3,17 @@ import { createTheme } from "@material-ui/core/styles";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#4caf50",
-      light: "#81c784",
-      dark: "#388e3c"
+      // Random colors I chose
+      // main: "#4caf50",
+      // light: "#81c784",
+      // dark: "#388e3c"
+
+      // Alternate option for theme from https://material.io/resources/color/#!/?view.left=0&view.right=0&primary.color=388E3C
+      main: "#388e3c",
+      light: "#6abf69",
+      dark: "#00600f"
     }
   }
 });
-
-// Alternate option for theme from https://material.io/resources/color/#!/?view.left=0&view.right=0&primary.color=388E3C
-// main: "#388e3c",
-// light: "#6abf69",
-// dark: "#00600f"
 
 export default theme;
