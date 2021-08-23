47677894cdf5c39901d0ddfd707c3aced98b83a0
Ben Cook <mcbeanoo@gmail.com>
2021-08-23 15:44:05 +1000

    add authenticated routes to router

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,14 +2,38 @@ import Account from "./components/Account/Account";
 import SignUp from "./components/Account/Signup";
 import Home from "./components/Home";
 import NewClient from "./components/NewClient/NewClient";
-import { Route, Switch } from "react-router-dom";
+import firebase from "firebase/app";
+import { ReactElement } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Redirect, Route, Switch, RouteProps } from "react-router-dom";
+
+type AuthenticatedRouteProps = RouteProps & {
+  user: firebase.User | undefined;
+  component: (props: RouteProps) => ReactElement;
+};
+
+const AuthenticatedRoute = (props: AuthenticatedRouteProps) => {
+  const { user, component: Component, ...rest } = props;
+  if (user) {
+    return <Component {...rest} />;
+  }
+
+  return <Redirect to="/account" />;
+};
 
 const Router = () => {
+  const [user] = useAuthState(firebase.auth());
+
   return (
     <Switch>
       <Route exact path="/signup" component={SignUp} />
       <Route exact path="/account" component={Account} />
-      <Route exact path="/client/new" component={NewClient} />
+      <AuthenticatedRoute
+        exact
+        path="/client/new"
+        component={NewClient}
+        user={user}
+      />
 
       <Route path="/" component={Home} />
     </Switch>
