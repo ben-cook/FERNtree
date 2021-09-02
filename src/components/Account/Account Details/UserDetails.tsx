05237cd311c04c0ff3bee4dc4c0c06eaffb75ab9
Ben Cook <mcbeanoo@gmail.com>
2021-09-02 12:14:37 +1000

    small code changes

--- src/components/Account/Account Details/UserDetails.tsx
+++ src/components/Account/Account Details/UserDetails.tsx
@@ -41,37 +41,35 @@ const UserDetails = (user: firebase.User) => {
   const [data, loading] = useDocumentData<User>(userDocumentReference);
 
   return (
-    <>
-      <Grid container>
-        <Grid item xs={12} sm={7}>
-          <Typography variant="h4" className={classes.title}>
-            Account Details
+    <Grid container>
+      <Grid item xs={12} sm={7}>
+        <Typography variant="h4" className={classes.title}>
+          Account Details
+        </Typography>
+        {!loading && (
+          <Typography variant="h6" className={classes.subtitle}>
+            {`You're logged in as ${data?.firstName} ${data?.lastName}.`}
           </Typography>
-          {!loading && (
-            <Typography variant="h6" className={classes.subtitle}>
-              {`You're logged in as ${data?.firstName} ${data?.lastName}.`}
-            </Typography>
-          )}
+        )}
 
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => firebase.auth().signOut()}
-            data-cy="logout"
-          >
-            Sign Out
-          </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => firebase.auth().signOut()}
+          data-cy="logout"
+        >
+          Sign Out
+        </Button>
 
-          <DeleteAccountButton />
+        <DeleteAccountButton />
 
-          <ChangeUserDetailsForm {...user} />
-        </Grid>
-
-        <Grid item xs={12} sm={5}>
-          <Dashboard {...user} />
-        </Grid>
+        <ChangeUserDetailsForm {...user} />
       </Grid>
-    </>
+
+      <Grid item xs={12} sm={5}>
+        <Dashboard {...user} />
+      </Grid>
+    </Grid>
   );
 };
 
--- src/components/Client/Client.tsx
+++ src/components/Client/Client.tsx
@@ -15,7 +15,7 @@ import { TextField } from "formik-material-ui";
 import { useSnackbar } from "notistack";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useDocumentData } from "react-firebase-hooks/firestore";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import * as Yup from "yup";
 
 interface FormValues {
@@ -52,8 +52,10 @@ const useStyles = makeStyles((theme) =>
 
 const Client = () => {
   const classes = useStyles();
+  const history = useHistory();
   const { clientId } = useParams<{ clientId: string }>();
   const { enqueueSnackbar } = useSnackbar();
+
   const isNewClient = clientId == "new";
 
   const [authUser, authLoading] = useAuthState(firebase.auth());
@@ -119,241 +121,240 @@ const Client = () => {
   };
 
   return (
-    <>
-      <Grid container justifyContent="center">
-        <Grid item xs={12} sm={8} md={6}>
-          <Typography variant="h4" className={classes.title}>
-            {isNewClient && "New Client Profile"}
-            {!isNewClient && `${clientData?.firstName} ${clientData?.lastName}`}
-          </Typography>
-          <Formik
-            initialValues={
-              isNewClient ? newClientInitialValues : existingClientInitialValues
+    <Grid container justifyContent="center">
+      <Grid item xs={12} sm={8} md={6}>
+        <Typography variant="h4" className={classes.title}>
+          {isNewClient && "New Client Profile"}
+          {!isNewClient && `${clientData?.firstName} ${clientData?.lastName}`}
+        </Typography>
+        <Formik
+          initialValues={
+            isNewClient ? newClientInitialValues : existingClientInitialValues
+          }
+          validationSchema={Yup.object().shape({
+            firstName: Yup.string(),
+            lastName: Yup.string(),
+            business: Yup.string(),
+            address: Yup.string(),
+            email: Yup.string().email(),
+            phone: Yup.string(),
+            payRate: Yup.string(),
+            jobStatus: Yup.string(),
+            notes: Yup.string()
+          })}
+          onSubmit={(
+            values: FormValues,
+            { setSubmitting }: FormikHelpers<FormValues>
+          ) => {
+            setSubmitting(true);
+
+            if (isNewClient) {
+              // We're using add() instead of set() because we want an auto-generated UUID
+              userReference
+                .collection("clients")
+                .add(values)
+                .then(() => {
+                  enqueueSnackbar("New client created!", {
+                    variant: "success"
+                  });
+                  history.push("/");
+                })
+                .catch((err) => {
+                  console.error(err);
+                  enqueueSnackbar("Something went wrong.", {
+                    variant: "error"
+                  });
+                })
+                .finally(() => setSubmitting(false));
+            } else {
+              // We're using set() to update an existing document
+              userReference
+                .collection("clients")
+                .doc(clientId)
+                .set(values, { merge: true })
+                .then(() => {
+                  enqueueSnackbar("Updated client details.", {
+                    variant: "success"
+                  });
+                })
+                .catch((err) => {
+                  console.error(err);
+                  enqueueSnackbar("Something went wrong.", {
+                    variant: "error"
+                  });
+                })
+                .finally(() => setSubmitting(false));
             }
-            validationSchema={Yup.object().shape({
-              firstName: Yup.string(),
-              lastName: Yup.string(),
-              business: Yup.string(),
-              address: Yup.string(),
-              email: Yup.string().email(),
-              phone: Yup.string(),
-              payRate: Yup.string(),
-              jobStatus: Yup.string(),
-              notes: Yup.string()
-            })}
-            onSubmit={(
-              values: FormValues,
-              { setSubmitting }: FormikHelpers<FormValues>
-            ) => {
-              setSubmitting(true);
-
-              if (isNewClient) {
-                // We're using add() instead of set() because we want an auto-generated UUID
-                userReference
-                  .collection("clients")
-                  .add(values)
-                  .then(() => {
-                    enqueueSnackbar("New client created!", {
-                      variant: "success"
-                    });
-                  })
-                  .catch((err) => {
-                    console.error(err);
-                    enqueueSnackbar("Something went wrong.", {
-                      variant: "error"
-                    });
-                  })
-                  .finally(() => setSubmitting(false));
-              } else {
-                // We're using add() instead of set() because we want an auto-generated UUID
-                userReference
-                  .collection("clients")
-                  .doc(clientId)
-                  .set(values, { merge: true })
-                  .then(() => {
-                    enqueueSnackbar("Updated client details.", {
-                      variant: "success"
-                    });
-                  })
-                  .catch((err) => {
-                    console.error(err);
-                    enqueueSnackbar("Something went wrong.", {
-                      variant: "error"
-                    });
-                  })
-                  .finally(() => setSubmitting(false));
-              }
-            }}
-          >
-            {({ isSubmitting, dirty }) => (
-              <Form>
-                <Grid container direction={"row"} spacing={2}>
-                  <Grid item xs={12} sm={6}>
-                    <Field
-                      component={TextField}
-                      variant={"outlined"}
-                      label={"First Name"}
-                      name={"firstName"}
-                      type={"text"}
-                      placeholder={"First Name"}
-                      fullWidth
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <Field
-                      component={TextField}
-                      variant={"outlined"}
-                      label={"Last Name"}
-                      name={"lastName"}
-                      type={"text"}
-                      placeholder={"Last Name"}
-                      fullWidth
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <Field
-                      component={TextField}
-                      variant={"outlined"}
-                      label={"Business"}
-                      name={"business"}
-                      type={"text"}
-                      placeholder={"Business"}
-                      fullWidth
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <Field
-                      component={TextField}
-                      variant={"outlined"}
-                      label={"Address"}
-                      name={"address"}
-                      type={"text"}
-                      placeholder={"Address"}
-                      fullWidth
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Field
-                      component={TextField}
-                      variant={"outlined"}
-                      label={"Category"}
-                      name={"category"}
-                      type={"text"}
-                      placeholder={"Category"}
-                      fullWidth
-                      select
-                    >
-                      {Object.keys(customCategories).map((categoryName) => (
-                        <MenuItem value={categoryName} key={categoryName}>
-                          {categoryName}
-                        </MenuItem>
-                      ))}
-                    </Field>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Typography variant="h5">Contact Information</Typography>
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <Field
-                      component={TextField}
-                      variant={"outlined"}
-                      label={"Email"}
-                      name={"email"}
-                      type={"text"}
-                      placeholder={"Email"}
-                      fullWidth
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <Field
-                      component={TextField}
-                      variant={"outlined"}
-                      label={"Phone Number"}
-                      name={"phone"}
-                      type={"text"}
-                      placeholder={"Phone Number"}
-                      fullWidth
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Typography variant="h5">Job Information</Typography>
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <Field
-                      component={TextField}
-                      variant={"outlined"}
-                      label={"Pay Rate"}
-                      name={"payRate"}
-                      type={"text"}
-                      placeholder={"Pay Rate"}
-                      fullWidth
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <Field
-                      component={TextField}
-                      variant={"outlined"}
-                      label={"Job Status"}
-                      name={"jobStatus"}
-                      type={"text"}
-                      placeholder={"Job Status"}
-                      fullWidth
-                      select
-                    >
-                      <MenuItem value="Not Started">Not Started</MenuItem>
-                      <MenuItem value="In Progress">In Progress</MenuItem>
-                      <MenuItem value="Completed">Completed</MenuItem>
-                    </Field>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Typography variant="h5">Additional Information</Typography>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Field
-                      component={TextField}
-                      variant={"outlined"}
-                      label={"Notes"}
-                      name={"notes"}
-                      type={"text"}
-                      placeholder={"Notes"}
-                      fullWidth
-                    />
-                  </Grid>
-
-                  {/* dynamic form fields occurs here - done by mapping category fields */}
-                  {category.map((attb, idx) => (
-                    <Grid item key={idx} xs={12}>
-                      <Field
-                        component={TextField}
-                        variant={"outlined"}
-                        label={attb}
-                        name={attb}
-                        type="text"
-                        placeholder={attb}
-                        fullWidth
-                      />
-                    </Grid>
-                  ))}
-
-                  <Box textAlign="center">
-                    <Button
-                      type={"submit"}
-                      variant={"contained"}
-                      color={"primary"}
-                      disabled={isSubmitting || !dirty}
-                      className={classes.submitButton}
-                    >
-                      {isNewClient && "Save"}
-                      {!isNewClient && "Update"}
-                    </Button>
-                  </Box>
+          }}
+        >
+          {({ isSubmitting, dirty }) => (
+            <Form>
+              <Grid container direction={"row"} spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <Field
+                    component={TextField}
+                    variant={"outlined"}
+                    label={"First Name"}
+                    name={"firstName"}
+                    type={"text"}
+                    placeholder={"First Name"}
+                    fullWidth
+                  />
                 </Grid>
-              </Form>
-            )}
-          </Formik>
-        </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Field
+                    component={TextField}
+                    variant={"outlined"}
+                    label={"Last Name"}
+                    name={"lastName"}
+                    type={"text"}
+                    placeholder={"Last Name"}
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Field
+                    component={TextField}
+                    variant={"outlined"}
+                    label={"Business"}
+                    name={"business"}
+                    type={"text"}
+                    placeholder={"Business"}
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Field
+                    component={TextField}
+                    variant={"outlined"}
+                    label={"Address"}
+                    name={"address"}
+                    type={"text"}
+                    placeholder={"Address"}
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    component={TextField}
+                    variant={"outlined"}
+                    label={"Category"}
+                    name={"category"}
+                    type={"text"}
+                    placeholder={"Category"}
+                    fullWidth
+                    select
+                  >
+                    {Object.keys(customCategories).map((categoryName) => (
+                      <MenuItem value={categoryName} key={categoryName}>
+                        {categoryName}
+                      </MenuItem>
+                    ))}
+                  </Field>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant="h5">Contact Information</Typography>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Field
+                    component={TextField}
+                    variant={"outlined"}
+                    label={"Email"}
+                    name={"email"}
+                    type={"text"}
+                    placeholder={"Email"}
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Field
+                    component={TextField}
+                    variant={"outlined"}
+                    label={"Phone Number"}
+                    name={"phone"}
+                    type={"text"}
+                    placeholder={"Phone Number"}
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant="h5">Job Information</Typography>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Field
+                    component={TextField}
+                    variant={"outlined"}
+                    label={"Pay Rate"}
+                    name={"payRate"}
+                    type={"text"}
+                    placeholder={"Pay Rate"}
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Field
+                    component={TextField}
+                    variant={"outlined"}
+                    label={"Job Status"}
+                    name={"jobStatus"}
+                    type={"text"}
+                    placeholder={"Job Status"}
+                    fullWidth
+                    select
+                  >
+                    <MenuItem value="Not Started">Not Started</MenuItem>
+                    <MenuItem value="In Progress">In Progress</MenuItem>
+                    <MenuItem value="Completed">Completed</MenuItem>
+                  </Field>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant="h5">Additional Information</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    component={TextField}
+                    variant={"outlined"}
+                    label={"Notes"}
+                    name={"notes"}
+                    type={"text"}
+                    placeholder={"Notes"}
+                    fullWidth
+                  />
+                </Grid>
+
+                {/* dynamic form fields occurs here - done by mapping category fields */}
+                {category.map((attb, idx) => (
+                  <Grid item key={idx} xs={12}>
+                    <Field
+                      component={TextField}
+                      variant={"outlined"}
+                      label={attb}
+                      name={attb}
+                      type="text"
+                      placeholder={attb}
+                      fullWidth
+                    />
+                  </Grid>
+                ))}
+
+                <Box textAlign="center">
+                  <Button
+                    type={"submit"}
+                    variant={"contained"}
+                    color={"primary"}
+                    disabled={isSubmitting || !dirty}
+                    className={classes.submitButton}
+                  >
+                    {isNewClient && "Save"}
+                    {!isNewClient && "Update"}
+                  </Button>
+                </Box>
+              </Grid>
+            </Form>
+          )}
+        </Formik>
       </Grid>
-    </>
+    </Grid>
   );
 };
 
