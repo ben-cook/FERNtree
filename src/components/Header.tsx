7b0ee440381adcb783399e7ef464e3ccfa6d24e9
Ben Cook <mcbeanoo@gmail.com>
2021-09-05 21:40:04 +1000

    small code changes + format code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,12 +9,10 @@ import {
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import EcoIcon from "@material-ui/icons/Eco";
-import PersonAddOutlinedIcon from '@material-ui/icons/PersonAddOutlined';
+import PersonAddOutlinedIcon from "@material-ui/icons/PersonAddOutlined";
+import firebase from "firebase/app";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useLocation, Link } from "react-router-dom";
-import Client from "./Client/Client";
-
-console.log("client: "+Client);
-//console.log("Path:" + window.location.pathname); 
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -24,8 +22,11 @@ const useStyles = makeStyles((theme: Theme) =>
     logoIcon: {
       marginLeft: theme.spacing(2)
     },
-    clientButton: {
-     // marginRight: theme.spacing(2)
+    newClientButton: {
+      marginRight: theme.spacing(2)
+    },
+    myClientsButton: {
+      marginRight: theme.spacing(2)
     },
     noTextDecoration: {
       textDecoration: "none",
@@ -36,6 +37,8 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const Header = () => {
   const classes = useStyles();
+  const location = useLocation();
+  const [user] = useAuthState(firebase.auth());
 
   const LogoIcon = () => (
     <IconButton color="inherit" size="medium" className={classes.logoIcon}>
@@ -49,20 +52,15 @@ const Header = () => {
     </IconButton>
   );
 
-  const ClientButton = () => (
-    <IconButton color="inherit" size="medium" className={classes.clientButton}>
+  const NewClientButton = () => (
+    <IconButton
+      color="inherit"
+      size="medium"
+      className={classes.newClientButton}
+    >
       <PersonAddOutlinedIcon fontSize="large" />
     </IconButton>
   );
-
-  const location = useLocation();
-  //console.log(location.pathname);
-
-  // const usePathname = () => {
-  //   const location = useLocation();
-  //   console.log(location.pathname);
-  // }
-  
 
   return (
     <AppBar position="static">
@@ -75,27 +73,26 @@ const Header = () => {
               </Link>
             </Grid>
             <Grid item>
-              
-              {/* My Clients Button only visible on account and any client pages */}
-              {( (location.pathname == "/account") || ((location.pathname.indexOf("/client")) > -1) ) && (
-                <Link
-                  to="/"
-                  className={classes.noTextDecoration}
-                >
-                  <Button variant="outlined" color = "inherit"> 
-                    My Clients 
+              {/* My Clients Button visible when logged in and not on home page. */}
+              {user && location.pathname != "/" && (
+                <Link to="/" className={classes.noTextDecoration}>
+                  <Button
+                    variant="outlined"
+                    color="inherit"
+                    className={classes.myClientsButton}
+                  >
+                    My Clients
                   </Button>
-                </Link>)}
+                </Link>
+              )}
 
-              {/* Add Client button only visible on account and any client pages but not new client page */}
-              {( (location.pathname == "/account") || ((location.pathname.indexOf("/client")) > -1) ) && (!(location.pathname == "/client/new") ) && (
-                <Link
-                  to="/client/new"
-                  className={classes.noTextDecoration}
-                >
-                  <ClientButton />
-                </Link>)}
-            
+              {/* Add Client button visible everywhere when logged in expect new client page */}
+              {user && location.pathname != "/client/new" && (
+                <Link to="/client/new" className={classes.noTextDecoration}>
+                  <NewClientButton />
+                </Link>
+              )}
+
               <Link
                 to="/account"
                 className={classes.noTextDecoration}
@@ -110,7 +107,5 @@ const Header = () => {
     </AppBar>
   );
 };
-
-
 
 export default Header;
