2530e0b5f08d1fa6d7ee470f4213fca3401f104a
kinderdropout <liamharding9@icloud.com>
2021-09-30 20:07:22 +1000

    Linting fixes

--- test/test.js
+++ test/test.js
@@ -1,6 +1,4 @@
-const assert = require('assert');
-const firebase = require('@firebase/testing');
-const { MyLocation } = require('@material-ui/icons');
+import { initializeTestApp, initializeAdminApp, assertSucceeds, assertFails } from '@firebase/testing';
 
 // variables to store various parameters
 const MY_PROJECT_ID = "mernda-express"
@@ -10,12 +8,12 @@ const myAuth = {uid: myId, email: "rick@rick.com"};
 
 // setup function for emulated database
 function getFirestore(auth) {
-    return firebase.initializeTestApp({projectId: MY_PROJECT_ID, auth: auth}).firestore();
+    return initializeTestApp({projectId: MY_PROJECT_ID, auth: auth}).firestore();
 }
 
 // create a test document as a database admin, bypassing security rules 
 function makeDocument(id) {
-    const admin = firebase.initializeAdminApp({projectId: MY_PROJECT_ID}).firestore();
+    const admin = initializeAdminApp({projectId: MY_PROJECT_ID}).firestore();
     admin.collection("users").doc(id).set({userId: anotherId});
 
 } 
@@ -26,14 +24,14 @@ describe("FERNTree", () => {
     it("Can write to a user document with the same ID as a user", async() => {
         const db = getFirestore(myAuth);
         const testDoc = db.collection("users").doc(myId);
-        await firebase.assertSucceeds(testDoc.set({foo: "bar"}));
+        await assertSucceeds(testDoc.set({foo: "bar"}));
     })
 
     it("Will deny a write operation if the user is not logged in", async() => {
         // no auth object is passed to represent unauthenticated access
         const db = getFirestore(null); 
         const testDoc = db.collection("users").doc("testies")
-        await firebase.assertFails(testDoc.set({foo: "bar"}))
+        await assertFails(testDoc.set({foo: "bar"}))
     })
 
     it("Will deny an attempt to write another user's data", async() => {
@@ -42,27 +40,27 @@ describe("FERNTree", () => {
         // create a database instance as authenticated user attempting to edit another document
         const db = getFirestore(myAuth);
         const testDoc = db.collection("users").doc(anotherId);
-        await firebase.assertFails(testDoc.set({foo: "baz"}));
+        await assertFails(testDoc.set({foo: "baz"}));
     })
 
     it("Will allow a user to read their own documents", async () => {
         const db = getFirestore(myAuth)
         const testDoc = db.collection("users").doc(myId)
-        await firebase.assertSucceeds(testDoc.get());
+        await assertSucceeds(testDoc.get());
     })
 
     it("Will deny a user reading another user's documents", async () => {
         makeDocument(anotherId);
         const db = getFirestore(myAuth)
         const testDoc = db.collection("users").doc(anotherId)
-        await firebase.assertFails(testDoc.get());
+        await assertFails(testDoc.get());
     })
 
     it("Will deny a read request when not authenticated", async () => {
         const db = getFirestore(null)
         // set these documents as admin
         const testDoc = db.collection("users").doc(myId);
-        await firebase.assertFails(testDoc.get());
+        await assertFails(testDoc.get());
     })
 
 })
