5369e2213c51bc715554c7d3dac4c9e96dbc3ae0
Ben Cook <mcbeanoo@gmail.com>
2021-10-01 22:10:33 +1000

    minor code cleanup

--- src/components/Client/CategorySelector.tsx
+++ src/components/Client/CategorySelector.tsx
@@ -1,61 +1,62 @@
 import { MenuItem, TextField } from "@material-ui/core";
 import { Field } from "formik";
 
+interface CategorySelectorProps {
+  selectedCategory: string;
+  handleChange: (s: string) => void;
+  categoryFields: string[];
+}
 
-export function CategorySelector(props: {
-  selectedCategory: string,
-  handleChange: (s: string) => void,
-  categoryFields: string[]
-
-}) {
-  
+export const CategorySelector = ({
+  selectedCategory,
+  handleChange,
+  categoryFields
+}: CategorySelectorProps) => {
   return (
-    <TextField 
-        variant={"outlined"}
-        label={"Category"}
-        name={"category"}
-        type={"text"}
-        placeholder={"Category"}
-        fullWidth
-        select
-        value={props.selectedCategory}
-        
-        onChange={(event) => props.handleChange(event.target.value)}
-        
+    <TextField
+      variant={"outlined"}
+      label={"Category"}
+      name={"category"}
+      type={"text"}
+      placeholder={"Category"}
+      fullWidth
+      select
+      value={selectedCategory}
+      onChange={(event) => handleChange(event.target.value)}
     >
-        {/* Allow user to select a category to apply to the client */}
-        {/* Map the names of each category into a dropdown menu */}
-        {props.categoryFields && props.categoryFields.map((category) => (
-            <MenuItem value={category} key={category}>
+      {/* Allow user to select a category to apply to the client */}
+      {/* Map the names of each category into a dropdown menu */}
+      {categoryFields &&
+        categoryFields.map((category) => (
+          <MenuItem value={category} key={category}>
             {category}
-            </MenuItem>
+          </MenuItem>
         ))}
     </TextField>
-    
-    
   );
+};
+
+interface CategorySelectorInputProps {
+  name: string;
+  categoryFields: string[];
 }
 
 // Formik-aware wrapper
-export function CategorySelectorInput(props: {
-        name: string, 
-        categoryFields: string[]}) {
-    return (
-        <Field name={props.name} id={props.name}>
-            {
-                
-                ({field: { value }, form: { setFieldValue }}) => 
-                    <div>
-                        <CategorySelector
-                            selectedCategory = {value}
-                            handleChange = {(category) => setFieldValue(props.name, category)}
-                            categoryFields={props.categoryFields}
-
-                        />
-                    </div>
-
-            }
-        </Field> 
-    );
-
-}
+export const CategorySelectorInput = ({
+  name,
+  categoryFields
+}: CategorySelectorInputProps) => {
+  return (
+    <Field name={name} id={name}>
+      {({ field: { value }, form: { setFieldValue } }) => (
+        <div>
+          <CategorySelector
+            selectedCategory={value}
+            handleChange={(category) => setFieldValue(name, category)}
+            categoryFields={categoryFields}
+          />
+        </div>
+      )}
+    </Field>
+  );
+};
