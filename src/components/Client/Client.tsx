9ebd910dc3153e39a7a90cf96f395b0495525923
Julie Zenou <jzenou@student.unimelb.edu.au>
2021-10-06 19:01:40 +1100

    tags appear and can be edited in client

--- src/components/Client/Client.tsx
+++ src/components/Client/Client.tsx
@@ -7,6 +7,7 @@ import { CustomCategory } from "../../types";
 import DeleteButtonWithDialog from "../DeleteButtonWithDialog";
 import Loading from "../Loading";
 import { CategorySelectorInput } from "./CategorySelector";
+import Tags from "../Home/Tags"
 import {
   Typography,
   makeStyles,
@@ -69,9 +70,18 @@ const Client = () => {
     .collection("clients")
     .doc(clientId);
 
-  const [clientData, clientLoading] = useDocumentData<ClientType>(
-    existingClientReference
-  );
+  const [clientData, clientLoading] = 
+    useDocumentData<ClientType>(existingClientReference);
+
+  // getting the client's tags
+  let tags: string[] = [];
+  if (clientData) {
+    if (clientData.tags) {
+      tags = clientData.tags;
+    } else {
+      tags = [];
+    }
+  }
 
   // Loading
   if (authLoading || categoriesLoading || clientLoading) {
@@ -174,10 +184,12 @@ const Client = () => {
   return (
     <Grid container justifyContent="center">
       <Grid item xs={12} sm={8} md={6}>
+
         <Typography variant="h4" className={classes.title}>
           {isNewClient && "New Client Profile"}
           {!isNewClient && `${clientData?.firstName} ${clientData?.lastName}`}
         </Typography>
+
         <Formik
           initialValues={
             isNewClient ? newClientInitialValues : existingClientInitialValues
@@ -270,7 +282,7 @@ const Client = () => {
                     placeholder={"Last Name"}
                     fullWidth
                   />
-                </Grid>
+                </Grid>         
                 <Grid item xs={12} sm={6}>
                   <Field
                     component={TextField}
@@ -383,7 +395,7 @@ const Client = () => {
                     placeholder={"Notes"}
                     fullWidth
                   />
-                </Grid>
+                </Grid>         
 
                 <Grid
                   container
@@ -410,8 +422,8 @@ const Client = () => {
                       <DeleteButtonWithDialog
                         buttonText="Delete Client"
                         dialogTitle="Delete Client?"
-                        dialogContent="Are you sure you wish to permanently delete this client? This
-            action cannot be reversed."
+                        dialogContent="Are you sure you wish to permanently delete this client? 
+                                      This action cannot be reversed."
                         deleteFunction={() => {
                           existingClientReference
                             .delete()
@@ -436,6 +448,22 @@ const Client = () => {
             </Form>
           )}
         </Formik>
+
+        {/*Tags section*/}
+        <Grid
+          container
+          //direction={'column'}
+          spacing = {10}
+          justifyContent="space-around"
+          alignItems="center"
+        >
+          <Grid item>
+            {clientData && 
+              (<Tags id={clientId} tags={tags} />
+            )}
+          </Grid>
+        </Grid>
+
       </Grid>
     </Grid>
   );
