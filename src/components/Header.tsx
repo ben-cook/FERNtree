b463f5c659a886ae41f0263eb16e40b83ebd6a9b
Ben Cook <mcbeanoo@gmail.com>
2021-10-12 16:49:06 +1100

    add mobile header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,15 +6,20 @@ import {
   AppBar,
   Container,
   Grid,
-  Typography
+  Typography,
+  useMediaQuery,
+  Drawer,
+  MenuItem
 } from "@material-ui/core";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import Brightness4Icon from "@material-ui/icons/Brightness4";
 import Brightness7Icon from "@material-ui/icons/Brightness7";
+import MenuIcon from "@material-ui/icons/Menu";
 import PersonAddOutlinedIcon from "@material-ui/icons/PersonAddOutlined";
 import { useTheme } from "@material-ui/styles";
 import firebase from "firebase/app";
+import { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useLocation, Link } from "react-router-dom";
 
@@ -35,6 +40,13 @@ const useStyles = makeStyles((theme: Theme) =>
     noTextDecoration: {
       textDecoration: "none",
       color: theme.palette.primary.contrastText
+    },
+    noTextDecorationDrawerItem: {
+      textDecoration: "none",
+      color: theme.palette.getContrastText(theme.palette.primary.contrastText)
+    },
+    drawerContainer: {
+      padding: "20px 30px"
     }
   })
 );
@@ -42,8 +54,11 @@ const useStyles = makeStyles((theme: Theme) =>
 const Header = ({ toggleTheme }: { toggleTheme: () => void }) => {
   const classes = useStyles();
   const location = useLocation();
-  const [user] = useAuthState(firebase.auth());
   const theme: Theme = useTheme();
+  const mobileView = useMediaQuery(theme.breakpoints.down("xs"));
+
+  const [open, setOpen] = useState<boolean>(false);
+  const [user] = useAuthState(firebase.auth());
 
   const ProfileButton = () => (
     <IconButton color="inherit" size="medium" className={classes.profileButton}>
@@ -62,6 +77,112 @@ const Header = ({ toggleTheme }: { toggleTheme: () => void }) => {
     </IconButton>
   );
 
+  if (mobileView) {
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <Container>
+            <Grid
+              container
+              justifyContent={"space-between"}
+              alignItems="center"
+            >
+              <Grid item>
+                <IconButton
+                  edge="start"
+                  color="inherit"
+                  aria-label="menu"
+                  aria-haspopup="true"
+                  onClick={() => setOpen(true)}
+                  style={{ marginRight: "1rem" }}
+                >
+                  <MenuIcon fontSize="large" />
+                </IconButton>
+              </Grid>
+              <Grid item>
+                <Link to="/" className={classes.noTextDecoration}>
+                  <img src={logo} style={{ width: "3rem", height: "3rem" }} />
+                </Link>
+              </Grid>
+            </Grid>
+
+            <Drawer anchor="left" open={open} onClose={() => setOpen(false)}>
+              <div className={classes.drawerContainer}>
+                <Link
+                  to={"/account"}
+                  className={classes.noTextDecorationDrawerItem}
+                >
+                  <MenuItem>
+                    <Typography variant="h6">My Account</Typography>
+                  </MenuItem>
+                </Link>
+                <Link to={"/"} className={classes.noTextDecorationDrawerItem}>
+                  <MenuItem>
+                    <Typography variant="h6">My Clients</Typography>
+                  </MenuItem>
+                </Link>
+                <Link
+                  to={"/client/new"}
+                  className={classes.noTextDecorationDrawerItem}
+                >
+                  <MenuItem>
+                    <Typography variant="h6">New Client</Typography>
+                  </MenuItem>
+                </Link>
+                <MenuItem>
+                  <div
+                    onClick={toggleTheme}
+                    className={classes.noTextDecorationDrawerItem}
+                  >
+                    {theme.palette.type === "light" ? (
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "row",
+                          alignItems: "center"
+                        }}
+                      >
+                        <Typography
+                          variant="h6"
+                          align="left"
+                          display="inline"
+                          style={{ marginRight: "0.4rem" }}
+                        >
+                          Light mode
+                        </Typography>
+                        <Brightness4Icon style={{ flexGrow: 1 }} />
+                      </div>
+                    ) : (
+                      <>
+                        <div
+                          style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            alignItems: "center"
+                          }}
+                        >
+                          <Typography
+                            variant="h6"
+                            align="left"
+                            display="inline"
+                            style={{ marginRight: "0.4rem" }}
+                          >
+                            Dark mode
+                          </Typography>
+                          <Brightness7Icon style={{ flexGrow: 1 }} />
+                        </div>
+                      </>
+                    )}
+                  </div>
+                </MenuItem>
+              </div>
+            </Drawer>
+          </Container>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -76,15 +197,26 @@ const Header = ({ toggleTheme }: { toggleTheme: () => void }) => {
               <IconButton
                 onClick={toggleTheme}
                 className={classes.noTextDecoration}
+                style={{ marginRight: "1rem" }}
               >
                 {theme.palette.type === "light" ? (
                   <>
-                    <Typography variant="body1">light mode</Typography>
+                    <Typography
+                      variant="body1"
+                      style={{ marginRight: "0.4rem" }}
+                    >
+                      light mode
+                    </Typography>
                     <Brightness4Icon />
                   </>
                 ) : (
                   <>
-                    <Typography>dark mode</Typography>
+                    <Typography
+                      variant="body1"
+                      style={{ marginRight: "0.4rem" }}
+                    >
+                      dark mode
+                    </Typography>
                     <Brightness7Icon />
                   </>
                 )}
